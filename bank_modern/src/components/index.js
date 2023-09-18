9d7c9a555173b976a8597f24dd14f2bd8e1dd1c6
ltuanghia265 <ltn26052003@gmail.com>
2023-09-19 01:23:35 +0700

    first commit

---
+++ bank_modern/src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Billing from "./Billing";
+import Business from "./Business";
+import Stats from "./Stats"
+import Footer from "./Footer"
+import CardDeal from "./CardDeal"
+import Hero from "./Hero"
+import Testimonials from "./Testimonials"
+
+export{
+    Navbar,
+    Billing,
+    Clients,
+    CTA,
+    Business,
+    Stats,
+    Footer,
+    CardDeal,
+    Hero,
+    Testimonials,
+};
